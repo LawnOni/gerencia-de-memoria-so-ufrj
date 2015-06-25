e9613d051671ad5373c840ee33a41740c909d6c0
Luan Martins <lawncm@gmail.com>
2015-06-25 16:31:49 -0300

    feito criaçao de processos

--- memoria.c
+++ memoria.c
@@ -7,57 +7,65 @@
 #include 	<time.h>
 #include	<sys/timeb.h>
 
-
-#define WORKSET_LIMIT 4;
-
-
+#define WORKSET_LIMIT 4
+#define FRAME_LIMIT 64
+#define VIRTUAL_MEMORY_SIZE 200
+#define THREAD_LIMIT 20
 
 struct page
 {
-	int process_id;
+	int *process_id;
     int number;
-    struct page *next;
+    int value;
 };
 
 struct process
 {
     int id;
+    struct page page_list[50];
 };
 
+int number_of_process = 0;
+struct process process_list[THREAD_LIMIT];
 
-// struct process[50] process_list;
-
-// struct page[64] main_memory;
-// struct page[200] virtual_memory;
+struct page main_memory[FRAME_LIMIT];
+struct page virtual_memory[VIRTUAL_MEMORY_SIZE];
 
 
 
 
 // Gerenciador de memória
 void request_page();
-
-
-
-
-//////////////////////////////////////////////////////
-
-void create_process();
+int create_process();
+void initialize_page_list(int size, int* process_id, struct page *page_list);
 
 //////////////////////////////////////////////////////
 	
 int main( int argc, char *argv[ ] ){
+	int process1_id = create_process();
 
-	struct page pagina1;
-	pagina1.process_id = 1;
-	pagina1.number = 1;
-
-	struct page pagina2;
-	pagina2.process_id =2;
-	pagina2.number=2; 
-
-	pagina1.next= &pagina2;
-
-	printf("\n%d", pagina1.number);
-	printf("\n%d", pagina1.next -> number);
+	printf("%d \n", process_list[process1_id].page_list[2].number);
 	return 0;
+}
+
+int create_process()
+{
+	struct process _process;
+	_process.id = number_of_process;
+	initialize_page_list(50, &number_of_process, _process.page_list);
+	process_list[number_of_process] = _process;
+	number_of_process++;
+
+	return _process.id;
+
+}
+
+void initialize_page_list(int size, int* process_id, struct page *page_list)
+{
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		page_list[i].process_id = process_id;
+		page_list[i].number = i;
+	}
 }
