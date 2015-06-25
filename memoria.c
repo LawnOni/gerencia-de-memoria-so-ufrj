a8b668d614379bc663b62172bf24f2fcfc275fcd
sevenleo <enfimleo@gmail.com>
2015-06-25 15:46:48 -0300

    teste de alocacao de pagina

--- memoria.c
+++ memoria.c
@@ -10,10 +10,7 @@
 
 #define WORKSET_LIMIT 4;
 
-// struct process[50] process_list;
 
-// struct page[64] main_memory;
-// struct page[200] virtual_memory;
 
 struct page
 {
@@ -27,8 +24,20 @@ struct process
     int id;
 };
 
+
+// struct process[50] process_list;
+
+// struct page[64] main_memory;
+// struct page[200] virtual_memory;
+
+
+
+
 // Gerenciador de memória
 void request_page();
+
+
+
 
 //////////////////////////////////////////////////////
 
@@ -38,12 +47,17 @@ void create_process();
 	
 int main( int argc, char *argv[ ] ){
 
-	struct page pagina;
-	pagina.process_id = 2;
-	pagina.number = 1;
-	pagina.next->number = 3;
+	struct page pagina1;
+	pagina1.process_id = 1;
+	pagina1.number = 1;
 
-	printf("%d", pagina.next->number);
+	struct page pagina2;
+	pagina2.process_id =2;
+	pagina2.number=2; 
 
+	pagina1.next= &pagina2;
+
+	printf("\n%d", pagina1.number);
+	printf("\n%d", pagina1.next -> number);
 	return 0;
 }
